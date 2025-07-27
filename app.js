922b717c1dce20f6c2d2ab9ba6353c663f2d2c92
havyas <havyasagaltha@gmail.com>
2025-07-27 21:28:48 +0530

    added all files

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require('cors')
+const app = express()
+const router = require('./routes/route')
+const connectDb = require('./connect/connect')
+require('dotenv').config()
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use('/api/tasks/', router)
+
+
+
+const start = async()=>{
+    try {
+        await connectDb(process.env.MONGO_URI)
+        app.listen(PORT,
+            console.log(`listening at port 5000`)
+        )
+ 
+    } catch (error) {
+        console.log("db is not connected")
+        
+    }
+
+}
+start()
